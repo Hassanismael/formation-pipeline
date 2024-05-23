a021b1272e3196f81e3f061a90de1df2f3292508
Your Name <you@example.com>
2024-05-23 16:26:36 +0000

    Premier commit

---
+++ src/main/java/com/example/formation/FormationApplication.java
@@ -0,0 +1,20 @@
+package com.example.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String Hello(){
+		return "<h2> Hello</h2>";
+	}
+}
